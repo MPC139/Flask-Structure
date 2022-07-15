562deca37a488db187f39a45a19483eade9397d3
Matias Pormi <mpormi@frba.utn.edu.ar>
2022-07-15 11:59:59 -0300

    Flask-WTF Froms added. I've created a small form to validate user and password then redirect to user interface.It's just for example and take this point to develop new features.

---
+++ app/main/forms.py
@@ -0,0 +1,11 @@
+from flask_wtf import Form
+from wtforms import StringField, SubmitField,PasswordField
+from wtforms.validators import Required
+
+
+
+class NameForm(Form):
+    name = StringField('What is your name?', validators=[Required()])
+    password = PasswordField('Pass',validators=[Required()])
+    submit = SubmitField('Submit')
+
--- app/main/views.py
+++ app/main/views.py
@@ -1,11 +1,22 @@
 from crypt import methods
+from tokenize import Name
+from unicodedata import name
 from . import main
 from flask import render_template, url_for, redirect
 from datetime import datetime
+from .forms import NameForm
 
-@main.route('/')
+@main.route('/',methods=['GET','POST'])
 def index():
-    return render_template('index.html',current_time=datetime.utcnow())
+    form = NameForm()
+    name = None
+    password = None
+    if form.validate_on_submit() and form.password.data == '1234':
+        name = form.name.data
+        form.name.data = ''
+        form.password.data = ''
+        return redirect(url_for('main.user',name=name))
+    return render_template('index.html',current_time=datetime.utcnow(),form=form)
 
 @main.route('/user/<name>')
 def user(name):
