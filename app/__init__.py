608f0f5a31ca6328bbcaea5f6b5fc10d36bb09d5
Matias Pormi <mpormi@frba.utn.edu.ar>
2022-07-15 10:22:18 -0300

    Added time feature to out WebApp

--- app/__init__.py
+++ app/__init__.py
@@ -1,13 +1,17 @@
 from flask import Flask
 from flask_bootstrap import Bootstrap
+from flask_moment import Moment
 
 bootstrap = Bootstrap()
+moment = Moment()
 
 def create_app(config_name):
     app = Flask(__name__)
 
     bootstrap.init_app(app)
+    moment.init_app(app)
 
+    
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
     
--- app/main/views.py
+++ app/main/views.py
@@ -1,12 +1,12 @@
 from crypt import methods
 from . import main
 from flask import render_template, url_for, redirect
+from datetime import datetime
 
-
-@main.route('/',methods=['GET'])
+@main.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html',current_time=datetime.utcnow())
 
-@main.route('/user/<name>',methods=['GET'])
+@main.route('/user/<name>')
 def user(name):
-    return render_template('user.html',name=name)
+    return render_template('user.html',name=name,current_time=datetime.utcnow())
