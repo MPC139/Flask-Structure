f1bc536f456788aff076b4c6ac7c7386f13aa7f3
Matias Pormi <mpormi@frba.utn.edu.ar>
2022-07-15 09:33:52 -0300

    Iss01 Resolved The problem was error 404 when we call favicon.ico. Resolution: The code is OK but when we run the command "flask run" to engine our web application, the system run on port 5000 by default. I ran the Web app on port 7000, 8200, 9000 and it work. I don't know why but all of this is resolved. Comments: We have to find out why this happen on different ports but for the moment I will leave this just it is.

--- app/main/views.py
+++ app/main/views.py
@@ -1,11 +1,12 @@
+from crypt import methods
 from . import main
-from flask import render_template
+from flask import render_template, url_for, redirect
 
 
-@main.route('/')
+@main.route('/',methods=['GET'])
 def index():
     return render_template('index.html')
 
-@main.route('/user/<name>')
+@main.route('/user/<name>',methods=['GET'])
 def user(name):
     return render_template('user.html',name=name)
