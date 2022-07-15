616d4c87ba70a63f3830f50f21fb635de6a90cd3
Matias Pormi <mpormi@frba.utn.edu.ar>
2022-07-15 11:12:53 -0300

    I added config option to make this app more safe. The main protection is against CSRF. This situation can be handle adding SECRET_KET by environ system.

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import os
 from app import create_app
 
 app = create_app('default') 
 
 if __name__=='__main__':
-    app.run(debug=True,port=8000)
+    print(os.environ.get('SECRET_KEY'))
+    app.run(port= 8000)
