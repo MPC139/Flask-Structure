51ea98aa606ef4af9d356e7fa5043fda19cb5c6a
Matias Pormi <mpormi@frba.utn.edu.ar>
2022-07-28 11:55:26 -0300

    Added Sending Asynchronous Email feature. More details in email.py

--- app/email.py
+++ app/email.py
@@ -1,6 +1,11 @@
 from . import mail
 from flask_mail import Message
 from flask import render_template, current_app
+from threading import Thread
+
+def send_async_email(app,msg):
+    with app.app_context():
+        mail.send(msg)
 
 def send_email(to, subject, template, **kwargs):
     app = current_app._get_current_object()
@@ -8,7 +13,10 @@ def send_email(to, subject, template, **kwargs):
                      sender=app.config['FLASKY_MAIL_SENDER'], recipients=[to])
     msg.body = render_template(template + '.txt', **kwargs)
     msg.html = render_template(template + '.html', **kwargs)
-    mail.send(msg)
+    thr = Thread(target= send_async_email, args=[app,msg])
+    thr.start()
+    return thr
+    
 
 
 # def send_email(to, subject, template, **kwargs):
